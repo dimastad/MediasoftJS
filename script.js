726c8884d61bad17a4480aa82982a71931362a2e
Dmitrii Portnov <dimastad29@gmail.com>
2019-07-08 22:56:18 +0400

    hometask2 - calculator

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert('Hello Mediasoft!');
+function myCalc() {
+    
+    let again = true;
+    
+    while( again === true){
+        let x,y,oper,result;
+        x=prompt("Введите первое числое", '');
+        oper=prompt("Введите операцию ' + ' либо ' - ' либо ' * ' либо ' / ' ", '');
+        y=prompt("Введите второе число", '');
+
+        if (oper==="+"){
+            result=Number(x)+Number(y);
+        }
+        else if (oper==="-"){
+            result=Number(x)-Number(y);
+        }
+        else if (oper==="*"){
+            result=Number(x)*Number(y);
+        }
+        else if (oper==="/"){
+        result=Number(x)/Number(y);
+        }
+        
+        alert ('Результат :' + ' ' + result);
+
+        again = confirm('Посчитать еще?');
+    }
+}
+
+myCalc();
