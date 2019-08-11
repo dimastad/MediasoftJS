5f252f1d1e8ce33a168aa528f142d51632af0e29
Dmitrii Portnov <dimastad29@gmail.com>
2019-08-12 01:15:53 +0400

    added order

--- the15puzzle/the15puzzle.js
+++ the15puzzle/the15puzzle.js
@@ -2,20 +2,32 @@ const isStart = confirm('Wanna begin new game?');
 
 
 if (isStart === true) {
-    console.log('Welcome to the game :)');
+    // console.log('Welcome to the game :)');
 
     const box = document.createElement('div');
     box.classList.add("playField");
     document.body.appendChild(box);
+    emptyCell = document.createElement('div');
+    emptyCell.classList.add('emptyCell');
+    // emptyCell.style.order = 16;
+    box.appendChild(emptyCell);
 
     let createCell = () => {
+        cellBox = document.querySelector('.playField');
+
         for ( i = 0; i < 15; i++ ) {
             cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.innerHTML = i + 1; 
-            cellBox = document.querySelector('.playField');
+            cell.innerHTML = i + 1;
+            cell.id = cell.innerHTML;
+            cell.style.order = cell.id;
             cellBox.appendChild(cell);
         }
+
+        const newOrder = Array.prototype.slice.call(box.getElementsByTagName('div'));
+        newOrder.sort(()=>Math.random() - 0.5); 
+        // console.log(newOrder);
+        
     }
 
     createCell();
